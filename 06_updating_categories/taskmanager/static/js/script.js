00107ed36fead0f6b74a9838cd438dab74cd1405
Tim Nelson <traveltimn@gmail.com>
2021-06-23 09:24:33 +0000

    Source Code - Lesson 06

---
+++ 06_updating_categories/taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+});
