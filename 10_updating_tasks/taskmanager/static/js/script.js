09d09e67839bf24e86af3a6b36079872c7c6698b
Tim Nelson <traveltimn@gmail.com>
2021-06-23 13:59:54 +0000

    Source Code - Lesson 10

---
+++ 10_updating_tasks/taskmanager/static/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+});
