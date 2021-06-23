4bf929937ead0ca99fa8cbb30e3558135605c6e4
Tim Nelson <traveltimn@gmail.com>
2021-06-23 09:11:23 +0000

    Source Code - Lesson 05

---
+++ 05_viewing_categories/run.py
@@ -0,0 +1,10 @@
+import os
+from taskmanager import app
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
---
+++ 05_viewing_categories/taskmanager/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template, request, redirect, url_for
+from taskmanager import app, db
+from taskmanager.models import Category, Task
+
+
+@app.route("/")
+def home():
+    return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
